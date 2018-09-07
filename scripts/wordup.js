521127e1028e01f04c00a32fc181dfb28543fcf9
Brian Schiller <bgschiller@gmail.com>
2018-09-06 20:37:59 -0600

    solution created in class

--- scripts/wordup.js
+++ scripts/wordup.js
@@ -1,270 +1,53 @@
 
+// ----------- Components -----------
 
-// ----------------- MODEL -----------------
+// The following are the code part of the <letter-chip>
+// and <submitted-word> components. They don't look like
+// much, because they don't _do_ very much right now.
+// However, if we wanted, these could have methods,
+// computed properties, and data just like the top-level
+// Vue that we've been using.
 
-var GAME_DURATION = 60;
+// The one thing these components have besides their
+// template is the `props` object. Props are a way of
+// passing in data to a custom component. Just like
+// we can write <input type="text">, and the 'type="text"'
+// bit changes the way the <input> looks and behaves,
+// props allow us to customize our components.
 
-// all the stuff we need to keep track of
-var model = {
-    // a boolean indicating whether the (first) game has started yet
-    gameHasStarted: false,
+Vue.component('letter-chip', {
+    /*
+    A letter-chip is one of the letters available to use in constructing words.
+    It displays the letter in large text and the value in smaller, all wrapped
+    in a tile-looking box.
+    */
+    template: '#letter-chip-template', // <-- This means: "use the element with selector '#letter-chip-template' as the template
+    props: {
+        letter: String,
+        value: Number,
+    },
+});
 
-    // how much time is left in the current game
-    secondsRemaining: GAME_DURATION,
-
-    // a list of the 7 letters that the player is allowed to use
-    allowedLetters: [],
-
-    // the word that the user is currently typing
-    currentAttempt: "",
-
-    // a list of the words the user has previously submitted in the current game
-    wordSubmissions: []
-}
-
-/*
- * Resets the model to a starting state, and then starts the timer
- */
-function startGame() {
-    endGame();
-    model.gameHasStarted = true;
-    model.secondsRemaining = GAME_DURATION;
-    model.allowedLetters = generateAllowedLetters();
-    model.wordSubmissions = [];
-    model.currentAttempt = "";
-    model.timer = startTimer();
-}
-
-/*
- * Wraps things up
- */
-function endGame() {
-    stopTimer();
-}
-
-
-/**
- * Given a word, adds a new wordSubmission to model.wordSubmissions.
- *
- * Refrains from adding a new entry if the model already contains
- * a wordSubmission whose word is the same
- */
-function addNewWordSubmission(word) {
-    // Do we already have a wordSubmission with this word?
-    // TODO 21
-    // replace the hardcoded 'false' with the real answer
-    var alreadyUsed = false;
-
-    // if the word is valid and hasn't already been used, add it
-    if (containsOnlyAllowedLetters(word) && alreadyUsed == false) {
-        model.wordSubmissions.push({ word: word });
-        // and now we must also determine whether this is actually a real word
-        checkIfWordIsReal(word);
-    }
-}
-
-/**
- * Given a word, checks to see if that word actually exists in the dictionary.
- *
- * Subsequently updates the .isRealWord property of
- * the corresponding wordSubmission in the model, and then re-renders.
- */
-function checkIfWordIsReal(word) {
-
-    // make an AJAX call to the Pearson API
-    $.ajax({
-        // TODO 13 what should the url be?
-        url: "www.todo13.com",
-        success: function(response) {
-            console.log("We received a response from Pearson!");
-
-            // let's print the response to the console so we can take a looksie
-            console.log(response);
-
-            // TODO 14
-            // Replace the 'true' below.
-            // If the response contains any results, then the word is legitimate.
-            // Otherwise, it is not.
-            var theAnswer = true;
-
-            // TODO 15
-            // Update the corresponding wordSubmission in the model
-
-
-            // re-render
-            render();
-        },
-        error: function(err) {
-            console.log(err);
-        }
-    });
-}
-
-
-// ----------------- VIEW -----------------
-
-/**
- * Updates everything on screen based on the current state of the model
- */
-function render() {
-
-    // PREGAME ---------------------------------
-
-    // update the score on the scoreboard
-    $("#current-score").text(currentScore());
-
-    // TODO 2
-    // Update the curent time remaining on the scoreboard.
-
-
-    // if the game has not started yet, just hide the #game container and exit
-    if (model.gameHasStarted == false) {
-        $("#game").hide();
-        return;
-    }
-
-    // GAME -------------------------------------
-
-    // clear stuff
-    $("#allowed-letters").empty();
-    $("#word-submissions").empty();
-    // TODO 10
-    // Add a few things to the above code block (underneath "// clear stuff").
-
-
-    // reveal the #game container
-    $("#game").show();
-
-    // render the letter tiles
-    var letterChips = model.allowedLetters.map(letterChip)
-    $("#allowed-letters").append(letterChips);
-
-    // TODO 11
-    // Render the word submissions
-
-
-    // Set the value of the textbox
-    $("#textbox").val(model.currentAttempt);
-    // TODO 3
-    // Give focus to the textbox.
-
-
-    // if the current word attempt contains disallowed letters,
-    var disallowedLetters = disallowedLettersInWord(model.currentAttempt);
-    if (disallowedLetters.length > 0) {
-        // restyle the textbox
-        $("#textbox").addClass("bad-attempt");
-
-        // show the disallowed letters underneath
-        var redLetterChips = disallowedLetters.map(disallowedLetterChip);
-
-        // TODO 8
-        // append the red letter chips to the form
-
-    }
-
-    // if the game is over
-    var gameOver = model.secondsRemaining <= 0
-    if (gameOver) {
-        // TODO 9
-        // disable the text box and clear its contents
-
-    }
-}
-
-
-/**
- * Given a letter, returns an HTML element which can be used to display
- * the letter as a large "chip" above the text box
- */
-function letterChip(letter) {
-    // a chip to display the letter
-    var letterChip = $("<span></span>")
-        .text(letter)
-        .attr("class", "tag tag-lg allowed-letter")
-
-    // a smaller chip to indicate how many points this letter is worth
-    var scoreChip = $("<span></span>")
-        .text(letterScore(letter))
-        .attr("class", "tag tag-default tag-sm");
-
-    return letterChip.append(scoreChip);
-}
-
-/**
- * Given a wordSubmission, returns an HTML element which can be used to display
- * the word as a large white "chip" below the text box.
- */
-function wordSubmissionChip(wordSubmission) {
-    var wordChip = $("<span></span>")
-        .text(wordSubmission.word)
-        .attr("class", "tag tag-lg word-submission");
-
-    // if we know the status of this word (real word or not), then add a green score or red X
-    if (wordSubmission.hasOwnProperty("isRealWord")) {
-        var scoreChip = $("<span></span>").text("⟐");
-        // TODO 17
-        // give the scoreChip appropriate text content
-
-        // TODO 18
-        // give the scoreChip appropriate css classes
-
-        // TODO 16
-        // append scoreChip into wordChip
-
-    }
-
-    return wordChip;
-}
-
-/**
- * Given a disallowed letter, returns a DOM element to display the letter
- * little red chip to display the letter
- */
-function disallowedLetterChip(letter) {
-    return $("<span></span>")
-        .text(letter)
-        .addClass("tag tag-sm tag-danger disallowed-letter");
-}
-
-
-// ----------------- DOM EVENT HANDLERS -----------------
-
-$(document).ready(function() {
-    // when the new game button is clicked
-    $("#new-game-button").click(function() {
-        // start the game and re-render
-        startGame();
-        render();
-    });
-
-    // TODO 6
-    // Add another event handler with a callback function.
-    // When the textbox content changes,
-    // update the .currentAttempt property of the model and re-render
-
-
-    // when the form is submitted
-    $("#word-attempt-form").submit(function(evt) {
-        // we don't want the page to refresh
-        evt.preventDefault();
-
-        // add a new word from whatever they typed
-        addNewWordSubmission(model.currentAttempt);
-
-        // clear away whatever they typed
-        model.currentAttempt = "";
-
-        // re-render
-        render();
-    });
-
-    // initial render
-    render();
+Vue.component('submitted-word', {
+    /*
+    A submitted-word is a word typed in while playing the game.
+    */
+    template: '#submitted-word-template',
+    props: {
+        word: String, // The word that was typed
+        score: Number, // The score (computed by adding the value of each letter)
+        loading: Boolean, // True/False whether we are still waiting for a response
+                          // from the Pearson API to say if this is a real word.
+        isRealWord: Boolean, // Not set unless finished loading.
+    },
 });
 
 
-// ----------------- GAME LOGIC -----------------
+// ----------- Game Logic -----------
+
+// Some helper functions for computing scores of letters and words.
+
+var GAME_DURATION = 60;
 
 // borrowing Scrabble's point system
 var scrabblePointsForEachLetter = {
@@ -272,43 +55,6 @@ var scrabblePointsForEachLetter = {
     n: 1, o: 1, p: 3, q: 10, r: 1, s: 1, t: 1, u: 1, v: 4, w: 4, x: 8, y: 4, z: 10
 }
 
-/**
- * Given a letter, checks whether that letter is "disallowed"
- * meaning it is not a member of the .allowedLetters list from the current model
- */
-function isDisallowedLetter(letter) {
-    // TODO 7
-    // This should return true if the letter is not an element of
-    // the .allowedLetters list in the model
-    return false;
-}
-
-/**
- * Given a word, returns a list of all the disallowed letters in that word
- * Note that the list might be empty, if it contains only allowed letters.
- */
-function disallowedLettersInWord(word) {
-    letters = word.split("");
-    return letters.filter(isDisallowedLetter);
-}
-
-/**
- * Given a word, returns true if the word is "clean",
- * i.e. the word does not contain any disallowed letters
- */
-function containsOnlyAllowedLetters(word) {
-    // TODO 12
-    // Return the actual answer.
-    return true;
-}
-
-/**
- * Returns a list of 7 randomly chosen letters
- * Each letter will be distinct (no repeats of the same letter)
- */
-function generateAllowedLetters() {
-    return chooseN(7, Object.keys(scrabblePointsForEachLetter));
-}
 
 /**
  * Given a letter, returns the score of that letter (case-insensitive)
@@ -330,7 +76,7 @@ function wordScore(word) {
     // TODO 19
     // Replace the empty list below.
     // Map the list of letters into a list of scores, one for each letter.
-    var letterScores = [];
+    var letterScores = letters.map(letterScore);
 
     // return the total sum of the letter scores
     return letterScores.reduce(add, 0);
@@ -341,20 +87,13 @@ function wordScore(word) {
  * Returns the user's current total score, which is the sum of the
  * scores of all the wordSubmissions whose word is a real dictionary word
  */
-function currentScore() {
+function currentScore(words) {
     // a list of scores, one for each word submission
-    var wordScores = model.wordSubmissions.map(function(submission) {
-        if (submission.isRealWord) {
-            return wordScore(submission.word);
-        }
-        else {
-            return 0;
-        }
-    });
+    var wordScores = words
+        .filter(submission => submission.isRealWord)
+        .map(submission => wordScore(submission.word));
 
-    // TODO 20
-    // return the total sum of the word scores
-    return 0;
+    return wordScores.reduce(add, 0);
 }
 
 
@@ -363,6 +102,9 @@ function currentScore() {
 /**
  * Randomly selects n items from a list.
  * Returns the selected items together in a (smaller) list.
+ *
+ * (I believe this is using an algorithm called
+ *  "Fisher-Yates Shuffle")
  */
 function chooseN(n, items) {
     var selectedItems = [];
@@ -383,33 +125,180 @@ function add(a, b) {
 }
 
 
-// ----------------- THE TIMER -----------------
+// ----------------- Main Vue -----------------
 
-// don't waste your brain power trying to understand how these functions work.
-// just use them
 
-/*
- * Makes the timer start ticking.
- * On each tick, updates the .secondsRemaining property of the model and re-renders.
- * Stops when model.secondsRemaining reaches 0.
- */
-function startTimer() {
-    function tick() {
-        return setTimeout(function() {
-            model.secondsRemaining = Math.max(0, model.secondsRemaining - 1);
-            render();
-            var stillTimeLeft = model.gameHasStarted && model.secondsRemaining > 0
-            if (stillTimeLeft) {
-                model.timer = tick();
+var app = new Vue({
+    el: '.mount-point',
+    data: function() {
+        // all the stuff we need to keep track of
+        return {
+            // how much time is left in the current game
+            secondsRemaining: GAME_DURATION,
+
+            // a list of the 7 letters that the player is allowed to use
+            allowedLetters: [],
+
+            // the word that the user is currently typing
+            currentAttempt: "",
+
+            // a list of the words the user has previously submitted in the current game
+            wordSubmissions: [],
+
+            // a timeoutID that can be used to clear a previously set timer
+            // (don't worry about this one, you won't have to mess with it)
+            timer: null,
+        };
+    },
+    computed: {
+        currentScore: function () {
+            return currentScore(this.wordSubmissions);
+        },
+        gameInProgress: function() {
+            return this.secondsRemaining > 0 && this.timer !== null;
+        },
+        disallowedLettersInWord: function() {
+            /**
+             * Given a word, returns a list of all the disallowed letters in that word
+             * Note that the list might be empty, if it contains only allowed letters.
+             */
+
+            var letters = this.currentAttempt.split("");
+            return letters.filter(letter => this.isDisallowedLetter(letter));
+        },
+
+        containsOnlyAllowedLetters: function() {
+            /**
+             * returns true if the currentAttempt is "clean",
+             * i.e. the word does not contain any disallowed letters
+             */
+            return this.disallowedLettersInWord.length === 0;
+        },
+
+    },
+    methods: {
+        isDisallowedLetter: function(letter) {
+            /**
+             * Given a letter, checks whether that letter is "disallowed"
+             * meaning it is not a member of the .allowedLetters list from the current data
+             */
+            return !this.allowedLetters.includes(letter);
+        },
+
+        letterScore: letterScore,
+
+        generateAllowedLetters: function() {
+            /**
+             * Returns a list of 7 randomly chosen letters
+             * Each letter will be distinct (no repeats of the same letter)
+             */
+            return chooseN(7, Object.keys(scrabblePointsForEachLetter));
+        },
+
+
+        startGame: function() {
+            // Resets the data to a starting state, and then starts the timer
+            console.log('inside start game');
+            this.endGame(); // terminate any existing games before starting a new one.
+            this.gameHasStarted = true;
+            this.secondsRemaining = GAME_DURATION,
+            this.allowedLetters = this.generateAllowedLetters();
+            this.wordSubmissions = [];
+            this.currentAttempt = '';
+            this.timer = this.startTimer();
+        },
+        endGame: function() {
+            this.stopTimer();
+        },
+        addNewWordSubmission: function(word) {
+            /**
+             * Given a word, adds a new wordSubmission to this.wordSubmissions.
+             *
+             * Refrains from adding a new entry if the model already contains
+             * a wordSubmission whose word is the same
+             */
+
+            // Do we already have a wordSubmission with this word?
+            // TODO
+            // replace the hardcoded 'false' with the real answer
+            var alreadyUsed = false;
+            if (this.containsOnlyAllowedLetters && !alreadyUsed) {
+                // add the word, with it's score.
+                // set loading to true, and isRealWord to null, to represent
+                // that we're not sure yet if this is a real word.
+                this.wordSubmissions.push({
+                    word: word,
+                    score: wordScore(word),
+                    loading: true,
+                    isRealWord: null,
+                });
+
+                // Now, check against the api to see if the word is real.
+                // when the api call comes back, we can update loading and isRealWord.
+                this.checkIfWordIsReal(word);
             }
-        }, 1000);
-    }
-    return tick();
-}
+            this.currentAttempt = '';
+        },
+        checkIfWordIsReal: function(word) {
+            /**
+             * Given a word, checks to see if that word actually exists in the dictionary.
+             *
+             * Subsequently updates the .isRealWord property of
+             * the corresponding wordSubmission in the data.
+             */
 
-/*
- * Makes the timer stop ticking.
- */
-function stopTimer() {
-    clearTimeout(model.timer);
-}
+            // TODO what should the url be?
+            fetch(`http://api.pearson.com/v2/dictionaries/lasde/entries?headword=${word}`)
+                .then(response => (response.ok ? response.json() : Promise.reject(response)))
+                .then(resp => {
+                    console.log("We received a response from Pearson!");
+
+                    // let's print the response to the console so we can take a looksie
+                    console.log(resp);
+
+                    // TODO 14
+                    // Replace the 'true' below.
+                    // If the response contains any results, then the word is legitimate.
+                    // Otherwise, it is not.
+                    var isARealWord = resp.count > 0;
+
+                    // TODO 15
+                    // Update the data to say that the word is real.
+                    // You'll have to find the correct entry in this.wordSubmissions,
+                    // and change it's loading and isRealWord values
+                    this.wordSubmissions.forEach((w) => {
+                        if (w.word === word) {
+                            w.isRealWord = isARealWord;
+                            w.loading = false;
+                        }
+                    });
+                })
+                .catch(error => console.error(error));
+        },
+
+
+        // ----------------- THE TIMER -----------------
+        // don't fret about these too hard. You can ignore these functions
+        // unless you really want to learn about them.
+
+        startTimer: function() {
+            this.stopTimer(); // in case it was already running.
+            this.secondsRemaining = GAME_DURATION;
+            // in 1 second, call tick
+            this.timer = setTimeout(() => this.tick(), 1000);
+        },
+        tick: function() {
+            // one second has passed
+            this.secondsRemaining -= 1;
+            if (this.secondsRemaining > 0) {
+                // in one second, call tick again
+                this.timer = setTimeout(() => this.tick(), 1000);
+            }
+        },
+        stopTimer: function() {
+            if (this.timer) {
+                clearTimeout(this.timer);
+            }
+        },
+    },
+});
